42ca92f8e410dae35da8a7dfa782ab1c1b0d93ab
Basten70 <90454559+Basten70@users.noreply.github.com>
2021-10-26 14:01:44 +0300

    Create js.js

---
+++ js.js
@@ -0,0 +1,16 @@
+function tt_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+  }
+   
+  function tt_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+  }
+  
+  function onClick(element) {
+    document.getElementById("img01").src = element.src;
+    document.getElementById("modal01").style.display = "block";
+    var captionText = document.getElementById("caption");
+    captionText.innerHTML = element.alt;
+  }
